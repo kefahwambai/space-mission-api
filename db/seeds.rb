b1e652b96d3f7a8f045a2026b0c6e2b9d27c6d47
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-09-11 20:17:17 +0300

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+
+# Scientist.create(name: 'Jane Doe', field_of_study: 'Astrophysics', avatar: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=922&q=80')
+# Scientist.create(name: 'John Smith', field_of_study: 'Biology', avatar: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80')
+# Scientist.create(name: 'Alice Johnson', field_of_study: 'Geology', avatar: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')
+
+# Planet.create(name: 'Mars', distance_from_earth: '225 million km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614728894747-a83421e2b9c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80')
+# Planet.create(name: 'Venus', distance_from_earth: '108 million km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614726365723-498aa67c5f7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1094&q=80')
+# Planet.create(name: 'Earth', distance_from_earth: '0 km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80')
+# Planet.create(name: 'Jupiter', distance_from_earth: '779 million km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614732414444-096e5f1122d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80')
+# Planet.create(name: 'Mercury', distance_from_earth: '91 million km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614724723656-457e78e0b50b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1117&q=80')
+# Planet.create(name: 'Saturn', distance_from_earth: '1.4 billion km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1637984135921-301a7d39e3b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1015&q=80')
+# Planet.create(name: 'Neptune', distance_from_earth: '4.5 billion km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614728423169-3f65fd722b7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80')
+# Planet.create(name: 'Uranus', distance_from_earth: '2.8 billion km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614732484003-ef9881555dc3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80')
+# Planet.create(name: 'Pluto', distance_from_earth: '5.9 billion km', nearest_star: 'Sun', image: 'https://images.unsplash.com/photo-1614314107768-6018061b5b72?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80')
+
+
+# Mission.create(name: 'Mars Exploration', scientist: Scientist.first, planet: Planet.first)
+# Mission.create(name: 'Venus Observation', scientist: Scientist.second, planet: Planet.second)
+# Mission.create(name: 'Jupiter Research', scientist: Scientist.third, planet: Planet.third)
+
